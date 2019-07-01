a41bf4ef4e958bb069d4cdf2a0a5f2b06ccfdbf1
Priyanka <Priyanka@DESKTOP-5EL6MDO>
2019-07-01 19:39:03 +0530

    First commit

---
+++ DataDrivenFramework/src/com/abc/magento/MagentoLogin.java
@@ -0,0 +1,41 @@
+package com.abc.magento;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MagentoLogin {
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream fis = new FileInputStream("C:\\Users\\HP ENVY\\Desktop\\Vemu\\DataDrivenFramework\\src\\com\\abc\\utilities\\DataDriven.properties");
+        Properties p = new Properties(); 
+        p.load(fis);
+        
+        String nurl = p.getProperty("url");
+        String nemail = p.getProperty("email");
+		String npassword = p.getProperty("password");
+		 
+		System.out.println(nurl);
+	    System.out.println(nemail);
+		System.out.println(npassword);
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(nurl);
+		
+		driver.findElement(By.linkText("My Account")).click();
+		driver.findElement(By.id("email")).sendKeys(nemail);
+		driver.findElement(By.id("pass")).sendKeys(npassword);
+		driver.findElement(By.id("send2")).click();
+		driver.findElement(By.linkText("Log Out")).click();
+		driver.close();
+	}
+
+}
